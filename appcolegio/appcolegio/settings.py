4be5946dc739003c4001f4a4ba122d50c36f3e16
groxemberg-proyec <groxemberg@gmail.com>
2025-07-10 18:46:39 -0300

    Instalación de djangorestframeword

--- appcolegio/appcolegio/settings.py
+++ appcolegio/appcolegio/settings.py
@@ -37,10 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'personas',
     'cursos',
     'calificaciones',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ["rest_framework.permissions.IsAuthenticated"],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
