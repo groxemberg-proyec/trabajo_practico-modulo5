fd50e2a6d4213f06b0c37363fac6a7d35fa76857
JaviSoria19 <javisoar.2002@gmail.com>
2025-07-10 22:46:21 -0400

    Se implementó los ModelViewSet de Django Rest Framework del módulo de gestión de Cursos

--- appcolegio/cursos/urls.py
+++ appcolegio/cursos/urls.py
@@ -1,5 +1,13 @@
-from   django.urls import path
+from   django.urls import include, path
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router = DefaultRouter()
+router.register(r'cursos', views.CursoViewSet)
+router.register(r'grados', views.GradoViewSet)
+router.register(r'paralelos', views.ParaleloViewSet)
+router.register(r'niveles', views.NivelViewSet)
+
 urlpatterns = [
-    path('cursos', views.cursos, name='cursos'),
+    path('', include(router.urls)),
 ]
--- appcolegio/cursos/views.py
+++ appcolegio/cursos/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+from .models import Curso, Grado, Paralelo, Nivel
+from .serializers import CursoSerializer, GradoSerializer, ParaleloSerializer, NivelSerializer
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+class GradoViewSet(viewsets.ModelViewSet):
+    queryset = Grado.objects.all()
+    serializer_class = GradoSerializer
+
+class ParaleloViewSet(viewsets.ModelViewSet):
+    queryset = Paralelo.objects.all()
+    serializer_class = ParaleloSerializer
+
+class NivelViewSet(viewsets.ModelViewSet):
+    queryset = Nivel.objects.all()
+    serializer_class = NivelSerializer
 
 # Create your views here.
 def cursos(request):
     return HttpResponse("Listado de cursos")
+
